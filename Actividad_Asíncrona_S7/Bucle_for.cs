d4682620879c14d0ea5ce998e271ae35df00bf7c
David Gómez <117203802+davidsgomez@users.noreply.github.com>
2023-03-08 22:02:30 -0600

    Add files via upload

---
+++ Actividad_Asíncrona_S7/Bucle_for.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Introducir un número entero: ");
+        int altura = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j <= i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
